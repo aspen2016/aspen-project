ad95e4c6c92864d7a640c0ed29efa81cfa5345bf
sen468 <sherin@ab.em-net.ne.jp>
2016-07-07 23:32:47 +0900

    会員登録です

---
+++ ASPEN_registration/thanks.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="Content-Security-Policy" content="default-src *; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+    <link rel="stylesheet" href="components/loader.css">
+    <script src="components/loader.js"></script>
+    <script>
+        //
+        // TODO: Write JavaScript code here
+        //
+    </script>
+    <title>新規会員登録</title>
+</head>
+<body>
+    <p>ユーザー登録が完了しました</p>
+    <p><a href="mypage.php">マイページへ</a></p>
+</body>
+</html>
